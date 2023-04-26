8cc1cee813d666a6bc0f51ff1daca4ea4a6feaf0
Olga Berezka <olga.berezka.main@gmail.com>
2023-04-26 16:16:34 +0100

    feat: add footer with operating system description

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const section = document.createElement('section');
 const textarea = document.createElement('textarea');
 const div = document.createElement('div');
 const h1 = document.createElement('h1');
+const footer = document.createElement('footer');
 
 body.append(header);
 body.append(main);
+body.append(footer);
 main.append(section);
 header.append(h1);
 section.append(textarea);
@@ -16,6 +18,10 @@ section.append(div);
 
 h1.innerText = 'Virtual Keyboard';
 h1.className = 'title';
+
 section.className = 'section';
 textarea.className = 'textarea';
 div.className = 'keyboard';
+
+footer.className = 'footer';
+footer.innerText = 'This keyboard is created on macOS operating system.\nTo change the language, use the key combination: Command + Space';
